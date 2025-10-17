d7402392a679fdbae0536a2efce81ce23c345e9a
Kshitij bajpai <sanjaikumarbajpai305@gmail.com>
2025-10-18 01:02:19 +0530

    Create q-2.cpp

---
+++ q-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                // Find position to fill node's value
+                int index = leftToRight ? i : size - i - 1;
+                level[index] = node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            ans.push_back(level);
+            leftToRight = !leftToRight; // flip direction
+        }
+
+        return ans;
+    }
+};
